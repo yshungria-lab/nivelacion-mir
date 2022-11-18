8ceabc88a2b8f924ceec91755c18556f25e3a238
Yojan Stiben Hungria <yojanstibenh@gmail.com>
2022-11-18 18:54:30 -0500

    Ejercicio Hola realizado

---
+++ ejercicio4/paso1.js
@@ -0,0 +1,11 @@
+//Crea una función llamada hola que reciba un argumento (una cadena de texto)
+//y retorne “Hola ” seguido del argumento y un signo de exclamación:
+
+function hola(cadena){
+    return "Hola " + cadena + "!";
+}
+
+console.log(hola("Pedro"));
+console.log(hola("Juan"));
+console.log(hola(""));
+
